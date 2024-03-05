e53409e46a7e08f0b4e8da5d0532858db47c4542
rita-kuo-jedisoft <rita.kuo@jedisoft.com>
2024-03-05 14:41:26 +0800

    feat: problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,156 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
+    <main className="bg-amber-50 p-4 min-h-screen">
+      <div className="p-4 max-w-[720px] mx-auto bg-white rounded shadow">
+        <h1 className="font-bold text-4xl text-center">
+          Frontend Engineer Assessment
+        </h1>
+        <p className="mt-6">
+          Welcome to our frontend engineer assessment! This test is designed to
+          evaluate your frontend development skills and related knowledge.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p className="mt-4">
+          Please read each question carefully and imagine them as typical work
+          tasks. Respond in the same manner you would during your regular work.
+        </p>
+        <p className="mt-4">
+          You can create any folders, ts/tsx/css files you need at any position
+          that you think is appropriate.
+        </p>
+        <p className="mt-4">Here's the requirements for the assessment:</p>
+        <ul className="list-decimal pl-6">
+          <li className="mt-1">
+            <strong>NEVER</strong> change anything in the directories named in
+            the pattern
+            <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+              **/api/**
+            </span>
+          </li>
+          <li className="mt-1">style with tailwindcss and css file.</li>
+          <li className="mt-1">
+            use
+            <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+              axios
+            </span>
+            to fetch data
+          </li>
+          <li className="mt-1">
+            <p>
+              Submit the URL of your answer repository by
+              <strong className="mx-1">
+                23:59 on the third day after receiving the questions,
+              </strong>
+              and cease answering (pushing commits). For example: if you receive
+              the questions on Mar 1st, you should submit the answer by 23:59 on
+              Mar 3rd.
+            </p>
+          </li>
+        </ul>
+        <p className="mt-4">Good luck! Let's get started.</p>
+        <div className="mt-6 border-t pt-6">
+          <h2 className="text-xl font-bold">Problem 1: Refactoring</h2>
+          <p className="mt-1">
+            For this task, your objective is to refactor the current page
+            following your coding conventions and best practices. Look for
+            opportunities to enhance code structure, eliminate redundancy,
+            clarify variable names, and simplify complex logic.
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="mt-6 border-t pt-6">
+          <h2 className="text-xl font-bold">Problem 2: Maze</h2>
+          <p className="mt-1">
+            For this task, your objective is to implement a page where a mouse
+            traverses a maze to find cheese.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          <p className="mt-4">
+            During server-side rendering, you need to hit the
+            <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+              /api/maze
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            endpoint to fetch the maze map array and display all the maps along
+            with their respective start buttons on the screen. When the user
+            clicks the start button, the mouse on that map will begin to solve
+            the maze using Depth-First Search (DFS), with each step taking 300
+            ms.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
+          <p className="mt-4">
+            Meanwhile, the start button will disappear, replaced by a reset
+            button. Clicking the reset button will stop the mouse's movement,
+            reset the map to its initial state, and the reset button will be
+            replaced by the start button again.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className="mt-4">Here is an example video:</p>
+          <p className="mt-4">Note that:</p>
+          <ul className="list-decimal pl-6">
+            <li className="mt-1">
+              Put this page at the route
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                /find-the-cheese
+              </span>
+              , and title it
+              <strong className="mx-1">'Find the cheese'.</strong>
+            </li>
+            <li className="mt-1">
+              The color of the walls is
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                green-800
+              </span>
+            </li>
+            <li className="mt-1">
+              The color of the path is
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                lime-50
+              </span>
+            </li>
+            <li className="mt-1">
+              The icon of the mouse is
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                LuRat
+              </span>
+              with color
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                neutral-500
+              </span>
+            </li>
+            <li className="mt-1">
+              The icon of the cheese is
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                FaCheese
+              </span>
+              with color
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                amber-400
+              </span>
+            </li>
+            <li className="mt-1">
+              Highlight current path with color
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                amber-200
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="mt-6 border-t pt-6">
+          <h2 className="text-xl font-bold">Problem 3: Layout</h2>
+          <p className="mt-1">
+            For this task, your objective is to create and apply a global shared
+            layout. The layout should consist of:
           </p>
-        </a>
+          <ul className="list-decimal pl-6">
+            <li className="mt-1">
+              A header displaying
+              <strong className="mx-1">'Jedi Software'</strong> that collapses
+              when scrolling up and reappears when scrolling stops.
+            </li>
+            <li className="mt-1">
+              A collapsible menu offering redirection to the homepage and
+              <span className="font-bold inline mx-1 py-0.5 px-1.5 border border-amber-400 bg-amber-50 rounded">
+                /find-the-cheese
+              </span>
+              .
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   );
